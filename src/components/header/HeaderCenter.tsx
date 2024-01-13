23374ff606e58e2d4bcbac94f615b4228fdc6338
Dmitriifd <idimka9@gmail.com>
2024-01-13 21:57:41 +0500

    feat: кнопка для выбора города в Header

--- src/components/header/HeaderCenter.tsx
+++ src/components/header/HeaderCenter.tsx
@@ -5,6 +5,8 @@ import { HeaderLogo } from './HeaderLogo';
 import { HeaderSearch } from './HeaderSearch';
 
 const HeaderCenter = () => {
+  const user = false;
+
   return (
     <div className="flex items-center">
       <HeaderLogo />
@@ -14,13 +16,17 @@ const HeaderCenter = () => {
           href="/profile"
           className="group border py-3 flex gap-2 rounded-md text-sm p-[18px] transition-colors hover:bg-accent-base">
           <UserIcon className="group-hover:fill-white fill-[#FE9015]" />
-          <span className="group-hover:text-white">Николай</span>
+          {user ? (
+            <span className="group-hover:text-white">Николай</span>
+          ) : (
+            <span className="group-hover:text-white">Войти</span>
+          )}
         </a>
         <a
           href="/profile"
           className="relative group border py-3 flex gap-2 rounded-md text-sm p-[18px] transition-colors hover:bg-accent-base">
           <BasketIcon className="group-hover:fill-white fill-[#FE9015]" />
-          <span className="absolute flex items-center justify-center text-white text-xs font-medium bg-[#ED1C22] rounded-full h-[15px] w-[15px] top-1 left-7">
+          <span className="absolute flex items-center justify-center text-white text-xs font-medium bg-[#EC2525] rounded-full w-[15px] h-[15px] top-1 left-7 py-[1px] px-[3px]">
             2
           </span>
           <span className="group-hover:text-white">7 777 ₽</span>
--- src/components/header/HeaderTop.tsx
+++ src/components/header/HeaderTop.tsx
@@ -1,8 +1,16 @@
+import ArrowIcon from '@assets/icons/arrowDown.svg?react';
+import GeoIcon from '@assets/icons/geo.svg?react';
 import PhoneIcon from '@assets/icons/phone.svg?react';
+import { Button } from '@components/ui/button';
 import { topNavLink } from '@mock/data';
 const HeaderTop = () => {
   return (
-    <nav className="flex justify-between py-4">
+    <nav className="flex items-center justify-between py-4">
+      <Button variant="search" className="flex gap-1">
+        <GeoIcon />
+        <span>Москва</span>
+        <ArrowIcon />
+      </Button>
       <ul className="flex gap-6 text-xs">
         {topNavLink.map(({ id, title, ulr }) => (
           <li key={id}>
@@ -14,13 +22,13 @@ const HeaderTop = () => {
       </ul>
       <ul className="flex gap-5">
         <li>
-          <a className="group transition-transform flex items-center gap-2 text-sm" href="tel:+73452594945">
+          <a className="group transition-transform flex items-center gap-2 text-sm" href="tel:+73534553322">
             <PhoneIcon className="group-hover:scale-[1.1] duration-300" />
             +7 (3534) 55-33-22
           </a>
         </li>
         <li>
-          <a className="text-sm text-accent-base" href="tel:+73452594945">
+          <a className="text-sm text-accent-base" href="tel:+73534553322">
             Заказать звонок
           </a>
         </li>
