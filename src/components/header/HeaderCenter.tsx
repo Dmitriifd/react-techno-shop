62e911e9e48359dad107de004dad18c32bd272d8
Dmitriifd <idimka9@gmail.com>
2024-01-13 23:50:25 +0500

    style: поменял ховер

--- src/components/header/HeaderCenter.tsx
+++ src/components/header/HeaderCenter.tsx
@@ -16,24 +16,20 @@ const HeaderCenter = () => {
       <div className="flex gap-4">
         <a
           href="/profile"
-          className="group border py-3 flex gap-2 rounded-md text-sm p-[18px] transition-colors hover:bg-accent-base">
-          <UserIcon className="group-hover:fill-white fill-[#FE9015]" />
-          {user ? (
-            <span className="hidden group-hover:text-white lg:block">Николай</span>
-          ) : (
-            <span className="hidden group-hover:text-white lg:block">Войти</span>
-          )}
+          className="border py-3 flex gap-2 rounded-md text-sm p-[18px] transition-colors hover:bg-accent hover:text-accent-foreground">
+          <UserIcon className="fill-[#FE9015]" />
+          {user ? <span className="hidden lg:block">Николай</span> : <span className="hidden lg:block">Войти</span>}
         </a>
         <a
           href="/profile"
-          className="relative group border py-3 flex gap-2 rounded-md text-sm p-[18px] transition-colors hover:bg-accent-base">
-          <BasketIcon className="group-hover:fill-white fill-[#FE9015]" />
+          className="relative border py-3 flex gap-2 rounded-md text-sm p-[18px] transition-colors hover:bg-accent hover:text-accent-foreground">
+          <BasketIcon className="fill-[#FE9015]" />
           <span className="absolute flex items-center justify-center text-white text-xs font-medium bg-[#EC2525] rounded-full w-[15px] h-[15px] top-1 left-7 py-[1px] px-[3px]">
             2
           </span>
-          <span className="hidden group-hover:text-white lg:block">7 777 ₽</span>
+          <span className="hidden lg:block">7 777 ₽</span>
         </a>
-        <Button variant="outline" className="py-3 flex h-[50px]">
+        <Button variant="outline" className="flex py-3 lg:hidden h-[50px]">
           <BurgerIcon />
         </Button>
       </div>
