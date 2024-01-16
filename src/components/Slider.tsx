e2f57b7e9ebb73ebe24c9eea1fe6ab228c67ca63
Dmitriifd <idimka9@gmail.com>
2024-01-16 15:59:23 +0500

    style: правка стилей для слайдера

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -12,19 +12,19 @@ export function Slider() {
           align: 'start',
         }}
         className="w-full">
-        <CarouselContent>
+        <CarouselContent className="-ml-1">
           {products.map((props) => (
             <CarouselItem
               key={props.id}
-              className="min-h-full sm:basis-[80%] sm2:basis-1/2 md:basis-1/3 xl:basis-1/4 xll:basis-1/5">
+              className="min-h-full basis-[80%] sm2:basis-1/2 md:basis-1/3 xl:basis-1/4 xll:basis-1/5 pl-1">
               <div className="p-1 h-full">
                 <ProductCard {...props} />
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious className="-left-3 disabled:hidden bg-white" />
-        <CarouselNext className="-right-3 disabled:hidden bg-white" />
+        <CarouselPrevious className="hidden -left-3 disabled:hidden bg-white sm2:flex" />
+        <CarouselNext className="hidden -right-3 disabled:hidden bg-white sm2:flex" />
       </Carousel>
     </div>
   );
