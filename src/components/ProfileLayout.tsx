c0c8bb7b50976073a81c9b52d5bba7dd26f6a567
Dmitriifd <idimka9@gmail.com>
2024-01-21 20:04:03 +0500

    feat: сменил Link на NavLink для страницы профиля
     - Добавил active класс для NavLink

--- src/components/ProfileLayout.tsx
+++ src/components/ProfileLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 import { Button } from '@components/ui/button';
 
@@ -12,21 +12,29 @@ const ProfileLayout = () => {
       <div className="flex gap-10">
         <ul className="space-y-4 border-r pr-10">
           <li>
-            <Link to=".">Учетная запись</Link>
+            <NavLink end to="." className={({ isActive }) => (isActive ? 'font-bold' : '')}>
+              Учетная запись
+            </NavLink>
           </li>
           <li>
-            <Link to="address">Адреса доставки</Link>
+            <NavLink to="address" className={({ isActive }) => (isActive ? 'font-bold' : '')}>
+              Адреса доставки
+            </NavLink>
           </li>
           <li>
-            <Link to="order" className="flex items-center">
+            <NavLink
+              to="order"
+              className={({ isActive }) => (isActive ? 'flex items-center font-bold' : 'flex items-center')}>
               <span className="pr-2">История заказов</span>
               <span className="h-4 w-4 rounded-full bg-[#ED1C22] inline-flex justify-center items-center text-white text-xs">
                 1
               </span>
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link to="password">Смена пароля</Link>
+            <NavLink to="password" className={({ isActive }) => (isActive ? 'font-bold' : '')}>
+              Смена пароля
+            </NavLink>
           </li>
           <li>
             <Button variant="link" className="p-0 h-fit text-[#EC2525] hover:no-underline">
