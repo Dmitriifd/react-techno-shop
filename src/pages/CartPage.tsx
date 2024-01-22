dddb1c8dc17b8b44464267a12faddc391deb73f2
Dmitriifd <idimka9@gmail.com>
2024-01-22 23:41:20 +0500

    feat: модальное окно для удаления товара из корзины

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,16 @@
 import { Link } from 'react-router-dom';
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import DeleteIcon from '@assets/icons/delete.svg?react';
 import MinusIcon from '@assets/icons/minus.svg?react';
 import PlusIcon from '@assets/icons/plus.svg?react';
@@ -48,10 +59,34 @@ const CartPage = () => {
                 </div>
                 {/* Buttons */}
                 <div>
-                  <button className="p-[5px] text-[#EC2525] text-sm inline-flex items-center gap-1 pl-0">
-                    <DeleteIcon />
-                    Удалить
-                  </button>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button
+                        variant="link"
+                        className="p-[5px] text-[#EC2525] text-sm inline-flex items-center gap-1 pl-0 hover:no-underline">
+                        <DeleteIcon />
+                        Удалить
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className="max-w-[460px]">
+                      <AlertDialogHeader className="mb-5">
+                        <AlertDialogDescription className="text-center">
+                          <img className="inline-block" width={200} height={200} src="/delete-cart.png" alt="sad cat" />
+                        </AlertDialogDescription>
+                        <AlertDialogTitle className="max-w-[340px] mx-auto font-medium">
+                          Вы уверены, что хотите удалить товар из корзины?
+                        </AlertDialogTitle>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter className="mx-auto">
+                        <AlertDialogCancel className="border-[#FE9015] text-accent-base w-[130px] hover:bg-transparent hover:text-accent-base">
+                          Отмена
+                        </AlertDialogCancel>
+                        <AlertDialogAction className="bg-[#EC2525] w-[130px] hover:bg-[#EC2525]/90">
+                          Удалить
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
               </div>
             </div>
@@ -83,10 +118,34 @@ const CartPage = () => {
                 </div>
                 {/* Buttons */}
                 <div>
-                  <button className="p-[5px] text-[#EC2525] text-sm inline-flex items-center gap-1 pl-0">
-                    <DeleteIcon />
-                    Удалить
-                  </button>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button
+                        variant="link"
+                        className="p-[5px] text-[#EC2525] text-sm inline-flex items-center gap-1 pl-0 hover:no-underline">
+                        <DeleteIcon />
+                        Удалить
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent className="max-w-[460px]">
+                      <AlertDialogHeader className="mb-5">
+                        <AlertDialogDescription className="text-center">
+                          <img className="inline-block" width={200} height={200} src="/delete-cart.png" alt="sad cat" />
+                        </AlertDialogDescription>
+                        <AlertDialogTitle className="max-w-[340px] mx-auto font-medium">
+                          Вы уверены, что хотите удалить товар из корзины?
+                        </AlertDialogTitle>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter className="mx-auto">
+                        <AlertDialogCancel className="border-[#FE9015] text-accent-base w-[130px] hover:bg-transparent hover:text-accent-base">
+                          Отмена
+                        </AlertDialogCancel>
+                        <AlertDialogAction className="bg-[#EC2525] w-[130px] hover:bg-[#EC2525]/90">
+                          Удалить
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
                 </div>
               </div>
             </div>
