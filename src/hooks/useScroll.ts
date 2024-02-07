2e5119655ed2d42a21574d9ae51be048b4c3b9a4
Dmitriifd <idimka9@gmail.com>
2024-02-07 13:31:50 +0500

    refactor: рефакторинг хука useScroll

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,31 +1,32 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export const useScroll = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const scrollPositionRef = useRef(scrollPosition);
 
   const handleScroll = useCallback(() => {
     const currentPosition = window.scrollY;
+    scrollPositionRef.current = currentPosition;
     setScrollPosition(currentPosition);
   }, []);
 
+  const checkWindowSize = useCallback(() => {
+    if (window.innerWidth >= 768) {
+      window.addEventListener('scroll', handleScroll);
+    } else {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [handleScroll]);
+
   useEffect(() => {
-    const checkWindowSize = () => {
-      if (window.innerWidth >= 768) {
-        window.addEventListener('scroll', handleScroll);
-      } else {
-        window.removeEventListener('scroll', handleScroll);
-      }
-    };
-
     checkWindowSize();
-
     window.addEventListener('resize', checkWindowSize);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
       window.removeEventListener('resize', checkWindowSize);
     };
-  }, [handleScroll]);
+  }, [checkWindowSize, handleScroll]);
 
   return scrollPosition;
 };
